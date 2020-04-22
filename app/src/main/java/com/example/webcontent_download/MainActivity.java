ccaa14df92eb67292713ca73ffea37dffb27d11b
prade091078 <prade.accet@gmail.com>
2020-04-22 21:27:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/webcontent_download/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.webcontent_download;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String,Void,String>
+    {
+        @Override
+        protected String doInBackground(String... urls) {
+             String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try
+            {
+             url=new URL(urls[0]);
+             urlConnection = (HttpURLConnection)url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while(data != -1)
+                {
+                    char current = (char) data;
+                    result += current;
+
+                    data = reader.read();
+                }
+
+            }
+            catch(Exception e)
+            { e.printStackTrace();
+            return "Failed";
+            }
+
+            return result;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        String result =null;
+        try {
+            result = task.execute("https://www.google.com//").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (result != null) {
+            Log.i("Result", result);
+        }
+    }
+}
